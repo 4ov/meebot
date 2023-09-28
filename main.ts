71cc87e3a3128664f6500a865dac19995cad36bd
Mohammed Amr <4ov@pm.me>
2023-09-28 15:55:39 +0300

    hello world

--- main.ts
+++ main.ts
@@ -1,40 +1,53 @@
-import { Bot, webhookCallback } from "npm:grammy"
-import { Hono } from "npm:hono"
+import "https://deno.land/std@0.203.0/dotenv/load.ts";
+import { Bot, webhookCallback } from "npm:grammy";
+import { Hono } from "npm:hono";
 
-function env(key: string){
-  const value = Deno.env.get(key)
-  if(value)return value
-  else{
-    throw new Error(`Key ${key} required but not found`)
+function env(key: string) {
+  const value = Deno.env.get(key);
+  if (value) return value;
+  else {
+    throw new Error(`Key ${key} required but not found`);
   }
 }
 
+const run_hash = "haha" + Math.random().toString(36).slice(0, 6);
 
-const bot = new Bot(env("BOT_TOKEN"))
-const app = new Hono()
+const bot = new Bot(env("BOT_TOKEN"));
+const app = new Hono();
 
-const BOT_SECRET = env("BOT_SECRET")
+const BOT_SECRET = env("BOT_SECRET");
 
-bot.on([":new_chat_members", ":left_chat_member"], async ctx=>{
+bot.on([":new_chat_members", ":left_chat_member"], async (ctx) => {
   //TODO: store it in a log
-  await ctx.deleteMessage().catch(e=>{
+  await ctx.deleteMessage().catch((e) => {
     console.log(e);
-  })
-})
+  });
+});
 
-
-bot.command("info", ctx=>{
-  ctx.reply(`<pre><code>ASUBot
+bot.command("info", (ctx) => {
+  ctx.reply(
+    `<pre><code>ASUBot
 host version: ${Deno.version.deno}
-</code></pre>`, {
-  parse_mode: "HTML",
-  protect_content: true
-})
-})
+hash: ${run_hash}
+</code></pre>`,
+    {
+      parse_mode: "HTML",
+      protect_content: true,
+    },
+  );
+});
 
-
-
-app.post(`/bot`,webhookCallback(bot, "hono", "return", 600, BOT_SECRET))
-
-
-Deno.serve(app.fetch)
+const s = Deno.serve({
+  port: 9000,
+}, (req) => {
+  if (req.url === "*") return new Response();
+  const url = new URL(req.url);
+  console.log(url.pathname);
+  if (url.pathname === "/bot") {
+    return webhookCallback(bot, "std/http", "return", 600, BOT_SECRET)(req);
+  } else {
+    return new Response("not found", {
+      status: 404,
+    });
+  }
+});
